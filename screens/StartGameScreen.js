b6625e405e5688fa7618b449beb80c4c10269e07
bitterkofte <hasantalhahtc@gmail.com>
2023-01-23 17:25:25 +0300

    first steps

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, TextInput, Pressable } from "react-native";
+
+function StartGameScreen() {
+    return(
+        <View>
+            <TextInput/>
+        </View>
+    )
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+
+});
